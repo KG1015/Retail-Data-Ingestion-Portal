38b6fdaa5228867e70734a8f5cbbdf45e12879ab
Krishna Gupta <kg1006.2003@gmail.com>
2026-04-03 18:34:12 +0530

    latest commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const uploadRoutes = require('./src/routes/upload.routes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
